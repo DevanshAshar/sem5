41d144433f38ce855510befafb90794c50c244bf
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-12-13 21:17:13 +0530

    did without oops

--- POA/lru.py
+++ POA/lru.py
@@ -1,22 +1,13 @@
 from collections import OrderedDict
-class LRUPageReplacement:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.page_order = OrderedDict()
-    def check_page_fault(self, page): 
-        if page in self.page_order: #HIT
-            self.page_order.move_to_end(page)          
-            return False  
+def lru_page_replacement(capacity, page_requests):
+    page_order = OrderedDict()
+    for page in page_requests:
+        if page in page_order:  # HIT
+            page_order.move_to_end(page)
+            print(f"Page {page} is already in the memory=> HIT")
         else:
-            if len(self.page_order) == self.capacity:
-                self.page_order.popitem(last=False) #last=False rakhenge toh FIFO order mein pop hoga
-            self.page_order[page] = None #dict hai isliye key ke saath kuch value assign karna padega
-            return True  # Page fault occurred
-capacity = 3 
-lru_algorithm = LRUPageReplacement(capacity)
-page_requests = [1,2, 3, 4, 1, 2, 5, 1, 2]
-for page in page_requests:
-    if lru_algorithm.check_page_fault(page):
-        print(f"Page {page} caused a page fault. Page order: {list(lru_algorithm.page_order.keys())}")
-    else:
-        print(f"Page {page} is already in the memory HIT . Page order: {list(lru_algorithm.page_order.keys())}")
+            if len(page_order) == capacity:
+                page_order.popitem(last=False)  #last=False rakhenge toh FIFO order mein pop hoga
+            page_order[page] = None #dict hai isliye key ke saath kuch value assign karna padega
+            print(f"Page {page} caused a page fault. Page order: {list(page_order.keys())}")
+lru_page_replacement(capacity = 3, page_requests = [1, 2,1, 3, 4, 1, 2, 5, 1, 2,7])
