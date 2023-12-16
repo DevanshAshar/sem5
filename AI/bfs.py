5fa65b48e3daf3e2ad03d477935fadce950c88fc
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-12-16 20:57:23 +0530

    Update bfs.py

--- AI/bfs.py
+++ AI/bfs.py
@@ -1,28 +1,32 @@
-def bfs(visited, graph, src, goal):
+def bfs(graph, src,goal,visited):
     visited.append(src)
-    queue.append(src)    
+    queue.append(src)
+    not_visited = [i for i in graph.keys() if i not in visited]   
+    print(f"{visited}\t\t{not_visited}\t\tFalse") 
     while queue:
-        m = queue.pop(0)
-        print(m)        
-        if m == goal:
-            break
+        m = queue.pop(0)     
         for i in graph[m]:
-            if i == " ": 
-                continue
-            elif i not in visited:
+            if i not in visited:
                 visited.append(i)
                 queue.append(i)
+                not_visited = [i for i in graph.keys() if i not in visited]
+                if goal in visited:
+                    print(f"{visited}\t\t{not_visited}\t\tTrue")                
+                    exit()
+                else:
+                    print(f"{visited}\t\t{not_visited}\t\tFalse")
 graph = {
-    "A": ["B", "C"],
-    "B": ["D", "E"],
-    "C": ["G"],
-    "D": [" "],
-    "E": ["F"],
-    "F": [" "],
-    "G": [" "],
+  'A': ['B','C'],
+  'B': ['A','D','E'],
+  'C': ['A','F','G'],
+  'D': [],
+  'E': [],
+  'F': ['H','I'],
+  'G': [],
+  'H': [],
+  'I': []
 }
-visited = []
 queue = []
 src = input("Enter source node: ")
 goal = input("Enter goal node: ")
-bfs(visited, graph, src, goal)
+bfs(graph, src, goal,visited=[])
