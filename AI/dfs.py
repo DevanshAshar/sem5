f452fa163983569d5e4235ee63de4a38f70dd8e3
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-12-16 18:30:39 +0530

    Update dfs.py

--- AI/dfs.py
+++ AI/dfs.py
@@ -1,21 +1,30 @@
-def dfs(graph, visited, src, goal):
-    if src not in visited:
-        visited.append(src)
-        if src == goal:
-            exit()
-        print(graph[src])
-        for i in graph[src]: #points to values of obj like agar src A hai toh ["B", "C"] ko point karega
-            dfs(graph, visited, i, goal)
 graph = {
-    "A": ["B", "C"],
-    "B": ["D", "E"],
-    "C": ["G"],
-    "D": [],
-    "E": ["F"],
-    "F": [],
-    "G": [],
+  'A': ['B', 'C'],
+  'B': ['A','D','E'],
+  'C': ['A','F','G'],
+  'D': [],
+  'E': [],
+  'F': ['H','I'],
+  'G': [],
+  'H': [],
+  'I': []
 }
+def dfs(graph, current_node, goal_node, visited, path):
+    if current_node not in visited:
+        path.append(current_node)
+        visited.append(current_node)
+        not_visited = [i for i in graph.keys() if i not in visited]
+        if current_node == goal_node:
+            print(f"{visited}\t\t{not_visited}\t\tTrue")
+            print(f"path : {path}")
+            exit()        
+        print(f"{visited}\t\t{not_visited}\t\tFalse")
+        for i in graph[current_node]:
+            dfs(graph, i, goal_node, visited, path)
+start_node = input("Enter start node : ")
+goal_node = input("Enter goal node : ")
 visited = []
-src = input("Enter source node: ")
-goal = input("Enter goal node: ")
-dfs(graph, visited, src, goal)
+path = []
+print("Visited\t\tNot Visted\t\tGoal state")
+print("<------------------------------------------------------------------------------>")
+dfs(graph, start_node, goal_node,visited,path)
