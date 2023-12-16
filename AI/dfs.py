852155896873ee0fecc47b38c3e57383af8309a0
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-12-16 19:47:07 +0530

    Update dfs.py

--- AI/dfs.py
+++ AI/dfs.py
@@ -9,22 +9,18 @@ graph = {
   'H': [],
   'I': []
 }
-def dfs(graph, current_node, goal_node, visited, path):
+def dfs(graph, current_node, goal_node, visited):
     if current_node not in visited:
-        path.append(current_node)
         visited.append(current_node)
         not_visited = [i for i in graph.keys() if i not in visited]
         if current_node == goal_node:
             print(f"{visited}\t\t{not_visited}\t\tTrue")
-            print(f"path : {path}")
             exit()        
         print(f"{visited}\t\t{not_visited}\t\tFalse")
         for i in graph[current_node]:
-            dfs(graph, i, goal_node, visited, path)
+            dfs(graph, i, goal_node, visited)
 start_node = input("Enter start node : ")
 goal_node = input("Enter goal node : ")
 visited = []
-path = []
 print("Visited\t\tNot Visted\t\tGoal state")
-print("<------------------------------------------------------------------------------>")
-dfs(graph, start_node, goal_node,visited,path)
+dfs(graph, start_node, goal_node,visited)
